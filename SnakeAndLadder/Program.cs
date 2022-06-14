5f0eff2c2f69ef3c9a61b68ebfdba6454fd16929
sahilkhan1996 <sahilkhan199655@gmail.com>
2022-06-14 23:02:56 +0530

    Welcome to Snake and ladder program

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    internal class SnakeAndLadder
+    {
+        static int playerposition;
+       
+        static void Main(string[] args)
+        {
+            //uc1: the position of player startng from 0
+            Console.WriteLine("The position of player is: " + playerposition);
+
+        }  
+
+    }
+}
