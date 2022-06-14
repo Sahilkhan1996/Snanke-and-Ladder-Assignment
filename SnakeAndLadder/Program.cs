8dbf34bccbc2ed936dea8afe3bd2dea6fb9b3995
sahilkhan1996 <sahilkhan199655@gmail.com>
2022-06-14 23:35:33 +0530

    Checking snake or ladder:UC3

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -16,6 +16,25 @@ namespace SnakeAndLadder
             Random r = new Random();
             int dienumber = r.Next(1, 7);
             Console.WriteLine("player Rolling die rolling and number comes is: " + dienumber);
+            //uc3: checking for option play, no play
+            Random r2 = new Random();
+            int gametime = r2.Next(0, 3);
+            Console.WriteLine("snake or ladder checker:" + gametime);
+            switch (gametime)
+            {
+                case 0:
+                    playerposition = playerposition + dienumber;
+                    Console.WriteLine("Next position of the player"+playerposition);
+                    break;
+                case 1:
+
+                    playerposition = playerposition - dienumber;
+                    Console.WriteLine("Next position of the player" + playerposition);
+                    break;
+                default:
+                    Console.WriteLine("No play");
+                    break;
+            }
 
         }
     }
