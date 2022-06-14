f9b7e947864aff215a7081ca996981b96ce6e809
sahilkhan1996 <sahilkhan199655@gmail.com>
2022-06-14 23:26:38 +0530

    Plater starting position

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -5,13 +5,14 @@ namespace SnakeAndLadder
     internal class SnakeAndLadder
     {
         static int playerposition;
-       
+
         static void Main(string[] args)
         {
             //uc1: the position of player startng from 0
             Console.WriteLine("The position of player is: " + playerposition);
-
-        }  
-
+            
+            
+            
+        }
     }
 }
